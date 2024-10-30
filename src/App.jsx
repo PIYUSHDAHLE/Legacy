3141356fdd67669241a1dde9d6d6c3e0fba9dc5b
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-10-30 13:10:59 +0530

    Page 1 and Page 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import Page1 from './components/home/Page1'
+import Page2 from './components/home/Page2'
+
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Page1/>
+      <Page2/>
     </div>
   )
 }
---
+++ src/components/home/Page1.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../home/page1.css'
+
+const Page1 = () => {
+  return (
+    <div className='box'>
+      <div className="content">
+          <h1>We Ensure better education for a better world</h1>
+          <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
+          <br />
+          <button className='btn'>Explore More</button>
+      </div>
+    </div>
+  )
+}
+
+export default Page1
